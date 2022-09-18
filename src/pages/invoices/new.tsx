96c7bfd24ec3a781f532caaae0fa462a8be38877
Jorge Humberto Nemogá Pinzón <jhnemogap@gmail.com>
2022-09-17 19:32:06 -0500

    feat: first approach to add new product of list

--- src/pages/invoices/new.tsx
+++ src/pages/invoices/new.tsx
@@ -1,16 +1,24 @@
 import {
   Box,
+  Button,
   FormControl,
   FormLabel,
   Heading,
+  HStack,
+  IconButton,
   Input,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
   NumberInputField,
-  NumberInputStepper, Textarea,
+  NumberInputStepper,
+  Textarea,
   VStack,
 } from '@chakra-ui/react';
+import { MinusIcon, PlusSquareIcon } from '@chakra-ui/icons';
+
+import { useEffect, useState } from 'react';
+import { JSXElement } from '@babel/types';
 
 function NewInvoicePage() {
   return (
@@ -19,7 +27,7 @@ function NewInvoicePage() {
         Crea una nueva factura
       </Heading>
 
-      <VStack spacing={4} alignItems='stretch'>
+      <VStack as='form' spacing={4} alignItems='stretch'>
         <FormControl>
           <FormLabel>FACTURA DE VENTA No.</FormLabel>
           <NumberInput
@@ -62,56 +70,7 @@ function NewInvoicePage() {
           <Input name='date-end' type='date' />
         </FormControl>
 
-        <VStack as='section' p={2} alignItems='stretch' bgColor='blue.500'>
-          <Heading as='h2' size='lg' mb={2} >
-            Elementos
-          </Heading>
-
-          <VStack as='article' p={2} spacing={2} bgColor='blue.700' borderRadius='0.5rem'>
-            <Heading as='h2' size='md'>
-              #1
-            </Heading>
-
-            <FormControl>
-              <FormLabel>Cantidad</FormLabel>
-              <NumberInput
-                name='amount'
-                defaultValue={0}
-                min={1}
-                max={99_999}
-                clampValueOnBlur={false}
-              >
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-
-            <FormControl>
-              <FormLabel>Valor Unitario</FormLabel>
-              <NumberInput
-                name='unit-value'
-                defaultValue={0}
-                min={1}
-                max={9_999_999}
-                clampValueOnBlur={false}
-              >
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-
-            <FormControl>
-              <FormLabel>Descripción producto / servicio</FormLabel>
-              <Textarea name='description' />
-            </FormControl>
-          </VStack>
-        </VStack>
+        <InvoiceProducts />
 
         <FormControl>
           <FormLabel>SON:</FormLabel>
@@ -123,3 +82,101 @@ function NewInvoicePage() {
 }
 
 export default NewInvoicePage;
+
+function InvoiceProducts() {
+  const [products, setProducts] = useState<unknown[]>([]);
+
+  const handleDeleteProduct = (index: number) => {
+    // setProducts((ps) => ps.filter((_, cIndex) => cIndex !== index - 1));
+  }
+
+  const handleAddProduct = () => {
+    setProducts((ps) => [...ps, null]);
+  }
+
+  return (
+    <VStack as='section' p={2} alignItems='stretch' spacing={4} bgColor='blue.500'>
+      {products.map((_, index) => (
+        <InvoiceProduct
+          key={`p-${index + 1}`}
+          index={index + 1}
+          onDelete={() => handleDeleteProduct(index + 1)}
+        />
+      ))}
+      <Button variant='ghost' leftIcon={<PlusSquareIcon />} onClick={handleAddProduct}>
+        Agregar producto
+      </Button>
+    </VStack>
+  );
+}
+
+function InvoiceProduct({ index = 1, onDelete = () => null }: InvoiceProductProps) {
+  return (
+    <VStack
+      as='article'
+      p={2}
+      spacing={2}
+      alignItems='stretch'
+      bgColor='blue.700'
+      borderRadius='0.5rem'
+    >
+      <HStack as='header' justifyContent='space-between'>
+        <Heading as='h2' size='md'>
+          Elemento # {index}
+        </Heading>
+        <IconButton
+          size='xs'
+          variant='solid'
+          colorScheme='red'
+          onClick={onDelete}
+          icon={<MinusIcon />}
+          aria-label='delete product'
+        />
+      </HStack>
+
+      <FormControl>
+        <FormLabel>Cantidad</FormLabel>
+        <NumberInput
+          name={`amount-e${index}`}
+          min={1}
+          max={99_999}
+          defaultValue={0}
+          clampValueOnBlur={false}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Valor Unitario</FormLabel>
+        <NumberInput
+          name={`unit-value-e${index}`}
+          min={1}
+          max={9_999_999}
+          defaultValue={0}
+          clampValueOnBlur={false}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Descripción producto / servicio</FormLabel>
+        <Textarea name={`description-e${index}`} defaultValue={''} />
+      </FormControl>
+    </VStack>
+  );
+}
+
+interface InvoiceProductProps {
+  index: number;
+  onDelete?: () => void;
+}
