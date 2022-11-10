3d9117f8e1e5f1647d3cb571b54adfa6543cfa6c
Jorge Humberto Nemogá Pinzón <jhnemogap@gmail.com>
2022-11-10 01:46:18 -0500

    fix: font and position of the invoice-number

--- src/pdf/makePdf.ts
+++ src/pdf/makePdf.ts
@@ -1,31 +1,47 @@
 import { jsPDF } from 'jspdf';
 
-import { pxToPt } from '../utils/lengthUnits.util';
+import { pxToCm, pxToPt } from '../utils/lengthUnits.util';
 
 import type { ResultSubmit } from 'pages/invoices/new';
 
 const COLOR_RED = '#f00';
+const COLOR_BLACK = '#000';
+const NORMAL_FONT_SIZE_IN_PX = 16; // 1.0rem -> 16px
+const SMALL_FONT_SIZE_IN_PX = NORMAL_FONT_SIZE_IN_PX * 0.75; // 0.75rem -> 12px
 
 export function generatePDF(props: generatePDFProps) {
   const { data, isPreview = false } = props;
   const doc = new jsPDF({ unit: 'cm', format: [19.9, 22.0] });
   // +++ app +++
+  doc.setLineWidth(0.01);
+  setBaseFontToFill({ doc });
   // doc.rect(0.7, 1.1, 18.5, 19.9);
-  doc.setFont('sans-serif', 'normal', 'normal');
   // +++ header +++
   drawInvoiceNumber({ doc, value: data.invoiceNumber });
   // +++ save or preview PDF +++
   return isPreview ? doc.output('bloburl') : doc.save('my-pdf-test');
 }
 
+// +++ header-item__bill-title +++
 function drawInvoiceNumber(props: DrawerFnValueString) {
   const { doc, value } = props;
-  // +++ header-item__bill-title +++
-  doc.rect(14.9, 1.4, 4.0, 0.9);
-  doc.setFont('Arial', 'normal', 'bold');
-  doc.setFontSize(pxToPt(24));
+  const [x, y] = [16.9, 2.1];
+  const fontSizeInPX = 24; // 1.5rem
+  const characterWidth = pxToCm(fontSizeInPX) / 2;
+  const shiftLeft = characterWidth * (value.length / 2);
+  // doc.rect(14.9, 1.4, 4.0, 1.0);
+  doc.setFont('helvetica', 'normal', 'bold');
+  doc.setFontSize(pxToPt(fontSizeInPX));
   doc.setTextColor(COLOR_RED);
-  doc.text(value, 14.9, 1.9);
+  doc.text(value, x - shiftLeft, y);
+  setBaseFontToFill({ doc });
+}
+
+function setBaseFontToFill(params: SetBaseFontToFillParams) {
+  const { doc, size = 'normal', fontWeight = 'normal' } = params;
+  doc.setTextColor(COLOR_BLACK);
+  doc.setFont('courier', 'normal', fontWeight);
+  doc.setFontSize(pxToPt(size === 'normal' ? NORMAL_FONT_SIZE_IN_PX : SMALL_FONT_SIZE_IN_PX));
 }
 
 interface DataSubmit {
@@ -42,4 +58,10 @@ interface BaseDrawerFn {
 
 interface DrawerFnValueString extends BaseDrawerFn {
   value: string;
+}
+
+interface SetBaseFontToFillParams {
+  doc: jsPDF;
+  size?: 'small' | 'normal';
+  fontWeight?: string | number;
 }
--- src/utils/lengthUnits.util.ts
+++ src/utils/lengthUnits.util.ts
@@ -2,25 +2,29 @@ export function pxToInches(px: number) {
   return px / 96;
 }
 
-export function inchesToPx(inches: number) {
-  return inches * 96;
-}
+// export function inchesToPx(inches: number) {
+//   return inches * 96;
+// }
 
 export function inchesToPt(inches: number) {
   return inches * 72;
 }
 
-export function ptToInches(pt: number) {
-  return pt / 72;
+// export function ptToInches(pt: number) {
+//   return pt / 72;
+// }
+
+// export function cmToPx(cm: number) {
+//   return (cm * 4800) / 127;
+// }
+
+export function pxToCm(px: number) {
+  return (px * 127) / 4800;
 }
 
-export function cmToPx(cm: number) {
-  return (cm * 4800) / 127;
-}
-
-export function cmToPt(cm: number) {
-  return inchesToPt(pxToInches(cmToPx(cm)));
-}
+// export function cmToPt(cm: number) {
+//   return inchesToPt(pxToInches(cmToPx(cm)));
+// }
 
 export function pxToPt(px: number) {
   return inchesToPt(pxToInches(px));
