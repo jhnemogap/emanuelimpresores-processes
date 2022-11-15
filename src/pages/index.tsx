0c90e269f448010e32eccd7f87385121c982fed9
Jorge Humberto Nemogá Pinzón <jhnemogap@gmail.com>
2022-11-14 19:29:12 -0500

    chore(release): 1.0.0 - basic generate pdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-import { Heading } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { Box, Button, Heading } from '@chakra-ui/react';
 
 function RootPage() {
+  const router = useRouter();
+
+  const handleGoToNewInvoice = () => router.push('/invoices/new');
+
   return (
-    <Heading as='h1' size='lg'>
-      ROOT PAGE
-    </Heading>
+    <Box as='main'>
+      <Heading as='h1' size='lg'>
+        ROOT PAGE
+      </Heading>
+      <Button variant='solid' colorScheme='blue' onClick={handleGoToNewInvoice}>
+        Ir a crear nueva factura
+      </Button>
+    </Box>
   );
 }
 
